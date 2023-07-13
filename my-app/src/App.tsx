9e8eaaf5ec914f821eb3c72877ce893b250f19ec
devgiants75 <devgiants75@naver.com>
2023-07-14 00:03:57 +0900

    20230714

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-import Components from './components';
-import Prop from './props/prop02';
-import State from './state';
-import Effect from './effect';
-import Hello from './props/hello';
-import Todo from './views/Practice';
-import Wrapper from './props/Wrapper';
-import Input from './views/Input';
-import ArrayRender from './views/ArrayRendering/application';
+import Components from "./components";
+import Prop from "./props/prop02";
+import State from "./state";
+import Effect from "./effect";
+import Hello from "./props/hello";
+import Todo from "./views/Practice";
+import Wrapper from "./props/Wrapper";
+import Input from "./views/Input";
+import ArrayRender from "./views/ArrayRendering/application";
+import Array from "./views/Array/Remove/Execute";
 
 function App() {
   return (
     <>
-    <State name='SeungAh'/>
-    <Effect />
-    <Todo />
-    {/* 
+      <State name="SeungAh" />
+      <Effect />
+      <Todo />
+      {/* 
     라우터 설치
     npm install react-router-dom
     
@@ -27,14 +28,20 @@ function App() {
     element속성: 컴포넌트를 지정
     */}
       <Routes>
-        <Wrapper>
-          <Route path='hello' element={<Hello color='red'/>}/>
-        </Wrapper>
-        <Route path='/' element={<Components />}/>
-        <Route path='hello' element={<Hello color='blue' name='Seugnah'/>}/>
-        <Route path='/props' element={<Prop name='Seungah'/>}/>
-        <Route path='/input' element={<Input />}/>
-        <Route path='/arrayRendering' element={<ArrayRender />}/>
+        <Route
+          path="hello"
+          element={
+            <Wrapper>
+              <Hello color="red" />
+            </Wrapper>
+          }
+        />
+        <Route path="/" element={<Components />} />
+        <Route path="hello" element={<Hello color="blue" name="Seugnah" />} />
+        <Route path="/props" element={<Prop name="Seungah" />} />
+        <Route path="/input" element={<Input />} />
+        <Route path="/arrayRendering" element={<ArrayRender />} />
+        <Route path="/array" element={<Array />} />
       </Routes>
     </>
   );
